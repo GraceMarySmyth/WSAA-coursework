0f4262908a345e0920fa1bac794563ef33a6bfbd
Grace Mary Smyth <157698316+GraceMarySmyth@users.noreply.github.com>
2025-03-25 12:24:37 +0000

    code in but need to do repo path

--- Assignments/assignment4-github.py
+++ Assignments/assignment4-github.py
@@ -14,3 +14,18 @@ repo = git.Repo(repo_path)
 file_path = os.path.join(repo_path, file_name)
 with open(file_path, 'r') as file:
     file_contents = file.read()
+
+# Replace the old name with the new name
+new_file_contents = file_contents.replace(old_name, new_name)
+
+# Write the new file contents back to the file
+with open(file_path, 'w') as file:
+    file.write(new_file_contents)
+
+# Commit the changes
+repo.index.add([file_name])
+repo.index.commit('Replaced instances of Andrew with Grace')
+
+# Push the changes
+origin = repo.remote(name='origin')
+origin.push()
