efca6e7d9582ffef0f576da2ddd6a06014639f5f
Grace Mary Smyth <157698316+GraceMarySmyth@users.noreply.github.com>
2025-03-25 11:49:55 +0000

    forgot double commas

--- Assignments/assignment3-cso.py
+++ Assignments/assignment3-cso.py
@@ -5,11 +5,11 @@ urlBegining= "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadData
 urlEnd= "/JSON-stat/2.0/en"
 
 def getAll(dataset):
+    url = urlBegining + dataset + urlEnd
     response = requests.get(url)
-    data = response.json()
-    return data
+    return response.json()
 
 if __name__ == "__main__":
     with open("cso.json", "wt") as fp:
-        print(json.dumps(getAll(FIQ02)), file=fp)
+        print(json.dumps(getAll("FIQ02")), file=fp)
  
