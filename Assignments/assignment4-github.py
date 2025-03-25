5b7633e7bddca103d8b231a95628de394495e522
Grace Mary Smyth <157698316+GraceMarySmyth@users.noreply.github.com>
2025-03-25 12:30:30 +0000

    relaised genders didnt change so added to code to fix this

--- Assignments/assignment4-github.py
+++ Assignments/assignment4-github.py
@@ -1,11 +1,15 @@
 import os
 import git
 
-# Define the repository path and file to modify and to modify the name Andrew
+# Define the repository path and files to modify and to modify the name Andrew
 repo_path = '/path/to/your/repository'
 file_name = 'assign4story.txt'
 old_name = 'Andrew'
 new_name = 'Grace'
+old_gender= 'he'
+new_gender= 'she'
+old_pronown= 'his'
+new_pronown= 'her'
 
 # Initialize the repository
 repo = git.Repo(repo_path)
@@ -17,6 +21,8 @@ with open(file_path, 'r') as file:
 
 # Replace the old name with the new name
 new_file_contents = file_contents.replace(old_name, new_name)
+new_file_contents = new_file_contents.replace(old_gender, new_gender)
+new_file_contents = new_file_contents.replace(old_pronown, new_pronown) 
 
 # Write the new file contents back to the file
 with open(file_path, 'w') as file:
