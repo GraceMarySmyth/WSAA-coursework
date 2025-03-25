a8a7e95c43e3b7519ae8c956d99a292458711401
Grace Mary Smyth <157698316+GraceMarySmyth@users.noreply.github.com>
2025-03-25 12:17:30 +0000

    Story for Assignment 4

--- Assignments/assignment3-cso.py
+++ Assignments/assignment3-cso.py
@@ -1,8 +1,13 @@
+from ast import Pass
 import requests
 import json
 
 urlBegining= "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/"
 urlEnd= "/JSON-stat/2.0/en"
+
+def getAllasFile(dataset):
+    with open("cso.json", "wt") as fp:
+        print(json.dumps(getAll(dataset)), file=fp)
 
 def getAll(dataset):
     url = urlBegining + dataset + urlEnd
@@ -10,6 +15,5 @@ def getAll(dataset):
     return response.json()
 
 if __name__ == "__main__":
-    with open("cso.json", "wt") as fp:
-        print(json.dumps(getAll("FIQ02")), file=fp)
+    getAllasFile("FIQ02")
  
