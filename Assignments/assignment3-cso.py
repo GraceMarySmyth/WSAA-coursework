5e720d3a0fcb871d1ad4f593f7afb7e1d0b7b266
Grace Mary Smyth <157698316+GraceMarySmyth@users.noreply.github.com>
2025-03-25 11:45:15 +0000

    rejigging URL

--- Assignments/assignment3-cso.py
+++ Assignments/assignment3-cso.py
@@ -1,14 +1,15 @@
 import requests
 import json
 
-url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/2.0/en"
+urlBegining= "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/"
+urlEnd= "/JSON-stat/2.0/en"
 
-def getAll():
+def getAll(dataset):
     response = requests.get(url)
     data = response.json()
     return data
 
 if __name__ == "__main__":
     with open("cso.json", "wt") as fp:
-        print(json.dumps(getAll()), file=fp)
+        print(json.dumps(getAll(FIQ02)), file=fp)
  
